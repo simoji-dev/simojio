61f7926ac03380d472e59cadb489db33c8e8e017
elmogit <christian.haenisch@posteo.de>
2022-04-01 15:13:35 +0200

    Update repo url in setup.py and path to Contributing.md

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ have to build it on a Windows system.
 
 ## Contributing
 
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
 
 ## Versioning
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=['numpy>=1.10.0', 'scipy>=0.19', 'matplotlib==3.2.2', 'lmfit>=0.9.12', 'pydicom', 'sympy>=0.7',
                       'numdifftools>=0.9', 'PySide2', 'emcee>=3.0.1', 'anytree', 'Cython', 'packaging', 'psutil',
                       'tmm'],
-    url='https://github.com/elmogit/simoji',
+    url='https://github.com/simoji-dev/simoji',
     license='LGPLv3',
     author='Christian Haenisch',
     author_email='christian.haenisch@tu-dresden.de',
